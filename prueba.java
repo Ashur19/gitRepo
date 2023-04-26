ef9f70e94c369f599399d1d39b57d7b2318a700f
Iglesias Differding <jordy.iglesias-differding@capgemini.com>
2023-04-26 15:11:11 +0200

    First Commit

---
+++ prueba.java
@@ -0,0 +1,15 @@
+
+public class prueba {
+	
+	private final String message;
+	
+	public prueba() {
+		this.message = "Hola mundo";
+		
+	}
+	
+	public string getMessage() {
+		
+		return message;
+	}
+}
